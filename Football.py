c9fb507db7688156a056eaef9f76007a660f00e1
Noham1412 <74252377+Noham1412@users.noreply.github.com>
2021-01-24 14:16:54 +0200

    Update Football.py

--- Football.py
+++ Football.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-# just making a small change123456578dfds32dfsddfsdfdfs
+# just making a small change123456578dfds32dfsddfsdfdfs4543
 class player:
     def __init__(self, name, attack, defence, shape, war, position):
         self.name = name
